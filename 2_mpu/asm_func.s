54a74a11e04beac1354e0e68152ce31e2aa12d1d
YIIIIIACH <1997marc212@gmail.com>
2019-05-31 02:06:15 +0800

    well done EZ

--- 2_mpu/asm_func.s
+++ 2_mpu/asm_func.s
@@ -2,24 +2,36 @@
 
 .global	read_sp
 read_sp:
-	??????
+	mov r0 , sp
+	blx lr
 
 .global	read_msp
 read_msp:
-	??????
-
+	mrs r0 , msp
+	blx lr
 .global	read_psp
 read_psp:
-	??????
+	mrs r0 , PSP
+	blx lr
 
 .global	read_ctrl
 read_ctrl:
-	??????
+	mrs r0, CONTROL
+	blx lr
 
 .global	start_user
 start_user:
-	??????
+	movs lr,r0
+	msr psp, r1
+	mov	r3,	#3
+	MSR CONTROL, r3 
+	
+	ISB
+	blx lr
 
 .global	sw_priv
 sw_priv:
-	??????
+	mov	r0,	#0
+	MSR CONTROL,    r0  
+	ISB
+	blx lr
